f1b1a13fbce01e8aa1e65c0d7486a65054e24098
horsefacegirl <51134090+horsefacegirl@users.noreply.github.com>
2020-01-23 11:03:23 +0100

    Card component from codalong

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Card } from "./lib/Card";
+import styled from "styled-components/macro";
+
+const ThinnerCard = styled(Card)`
+  width: 400px;
+`;
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <ThinnerCard
+        coverImage="https://www.fillmurray.com/500/300"
+        thumbnailUrl="https://www.fillmurray.com/100/100"
+        title="This is the title"
+        secondaryText="Life is amazing"
+      />
+      <Card title="Only a title" secondaryText="And some text" />
+      <Card>
+        <h1>Hello from children</h1>
+      </Card>
     </div>
-  )
-}
+  );
+};
